0679154d3cd3ccdf2945e3d0d1f07e2093758cf9
srivi10 <asrivignesh10@gmail.com>
2025-01-04 16:31:20 -0500

    Initial commit with Jenkinsfile, Dockerfile, and jenkinsFile.template

---
+++ src/main/java/org/jenkins/HelloWorld.java
@@ -0,0 +1,9 @@
+package org.jenkins;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HelloWorld {
+    public static void main(String[] args) {
+               System.out.printf("Hello and welcome!");
+       }
+}
